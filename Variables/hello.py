8ba10df8f90dfa0ef683bf40bf99d8dedf6afcbb
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-05 20:59:15 +0200

    Enhanced version

--- Variables/hello.py
+++ Variables/hello.py
@@ -1,1 +1,8 @@
-print("hello, world")
+# Ask user for their name
+name = input("What's your name? ").strip().title()
+
+# Get the first name only
+first, last = name.split()
+
+# Say hello to user
+print(f"hello, {first}")
