e07d19dcbfb032e33ef587cf6a17986a4a46bc70
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-07 08:06:33 +0200

    sixth program

--- Variables/shows.py
+++ Variables/shows.py
@@ -1,1 +1,21 @@
+SHOWS = [
+    " Avatar: the last airbender",
+    "Ben 10",
+    "Arthur",
+    " Spongebob Squarepants",
+    "Phineas and ferb",
+    "Kim possible",
+    "Jimmy Neutron ",
+    "the Proud family"
+]
 
+
+def main():
+    cleaned_shows = []
+    for show in SHOWS:
+        cleaned_shows.append(show.strip().title())
+    
+    print(', '.join(cleaned_shows))
+
+
+main()
