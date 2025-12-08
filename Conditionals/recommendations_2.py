6ea617823e6520fdb4fe90b70be6e812a06797aa
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-08 20:17:25 +0200

    Eighth program

--- Conditionals/recommendations_2.py
+++ Conditionals/recommendations_2.py
@@ -1,1 +1,25 @@
+def main():
+    difficulty = input("Difficult or Casual? ")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print("Enter a valid difficulty")
+        return
+    
+    player = input("Multiplayer or Single-player? ")
+    if not (player == "Multiplayer" or difficulty == "Single-player"):
+        print("Enter a valid number of players")
+        return
 
+    if difficulty == "Difficult" and player == "Multiplayer":
+        recommend("Poker")
+    elif difficulty == "Difficult" and player == "Single-player":
+        recommend("Klondike")
+    elif difficulty == "Casual" and player == "Multiplayer":
+        recommend("Hearts")
+    else:
+        recommend("Clock")
+
+def recommend(game):
+    print("You might like", game)
+
+
+main()
