6f9c701591624b931b9eb9d7cb04ef159d2e1a93
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-10 09:18:37 +0200

    Edit

--- Loops/dictionaries/report.py
+++ Loops/dictionaries/report.py
@@ -1,2 +1,17 @@
-def report():
-  ...
+def main():
+    spacecraft = {"name": "James Webb Space Telescope"}
+    spacecraft.update({"distance": 0.01, "orbit": "Sun"})
+    print(create_report(spacecraft))
+
+def create_report(spacecraft):
+    return f"""
+    ========= REPORT =========
+
+    Name: {spacecraft.get("name", "Unknown")}
+    Distance: {spacecraft.get("distance", "Unknown")} AU
+    Orbit: {spacecraft.get("orbit", "Unknown")}
+
+    ========= REPORT =========
+    """
+
+main()
