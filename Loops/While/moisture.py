0c61b6565d10250f6a6e0e6df2d3256362da21d7
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-10 15:49:00 +0200

    Edit

---
+++ Loops/While/moisture.py
@@ -0,0 +1,16 @@
+from soil import sample
+
+def main():
+    moisture = sample()
+    days = 1
+    print(f"Day {days}: Moisture is {moisture}%.")
+
+    while moisture > 20:
+        moisture = sample()
+        days += 1
+        print(f"Day {days}: Moisture is {moisture}%.")
+
+    print("Time to water!")
+
+
+main()
