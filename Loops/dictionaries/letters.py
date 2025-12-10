20067d102824697e10d70ad25dd3ae88bddabbc8
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-10 11:17:30 +0200

    Edit

--- Loops/dictionaries/letters.py
+++ Loops/dictionaries/letters.py
@@ -1,1 +1,21 @@
+def main():
+    names = ["Mario", "Luigi", "Daisy", "Yoshi"]
 
+    for name in names:
+        print(write_letter(name, "Princess Peach"))
+
+
+def write_letter(receiver, sender):
+    return f"""
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+        Dear {receiver},
+
+        You are invited.
+
+        Sincerely,
+        {sender}
+    +~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~+
+    """
+
+
+main()
