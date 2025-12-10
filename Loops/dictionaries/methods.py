3d3c10734ae535193df52875e4c8481fc645d1bc
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-10 10:11:00 +0200

    Edit

--- Loops/dictionaries/methods.py
+++ Loops/dictionaries/methods.py
@@ -1,1 +1,21 @@
+WORDS = {"PAIR": 4, "HAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
 
+def main():
+    print("Welcome to Spelling Bee!")
+    print("Your letters are: A I P C R H G")
+
+    while len(WORDS) > 0:
+        print(f"{len(WORDS)} left!")
+        guess = input("Guess a word: ")
+
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You've won!")
+        elif guess in WORDS.keys():
+            points = WORDS.pop(guess)
+            print(f"Good job! You scored {points} points.")
+
+    print("That's the game!")
+
+        
+main()
