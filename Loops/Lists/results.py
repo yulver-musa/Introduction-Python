ccbe3450a9e1286fa6364c3fb8751a82c0c35ee6
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2025-12-10 12:16:07 +0200

    Edit

--- Loops/Lists/results.py
+++ Loops/Lists/results.py
@@ -1,1 +1,18 @@
+results = ["Mario", "Luigi"]
 
+results.append("Princess")
+results.append("Yoshi")
+results.append("Koopa Troopa")
+results.append("Toad")
+
+results.append(["Bowser", "Donkey Kong Jr."])
+results.remove(["Bowser", "Donkey Kong Jr."])
+results.extend(["Bowser", "Donkey Kong Jr."])
+
+results.remove("Bowser")
+results.insert(0, "Bowser")
+
+results.append(input("Next competitor: "))
+results.reverse()
+
+print(results)
