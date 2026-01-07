0b0432a9f6072dfb57b0b0baff8f4fdbc83eccc1
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-01-07 07:19:45 +0200

    Edit

--- LIbraries/figlet.py
+++ LIbraries/figlet.py
@@ -2,12 +2,13 @@ from pyfiglet import Figlet
 import sys
 import random
 
+
 f = Figlet()
 fonts = f.getFonts()
 
 if len(sys.argv) == 1:
     font = random.choice(fonts)
-elif len(sys.argv) == 3 and sys.argv[1] in ["-f", "--font"] and sys.argv[2] in fonts:
+elif len(sys.argv) == 3 and sys.argv[1] in  ["-f", "--font"] and sys.argv[2] in fonts:
     font = sys.argv[2]
 else:
     sys.exit("Invalid usage")
@@ -16,6 +17,4 @@ f.setFont(font=font)
 
 text = input("Input: ")
 
-ascii_art = f.renderText(text)
-
-print(ascii_art)
+print(f.renderText(text))
