597573a6b10271490b7d3032579fbaec19849aa9
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-01-10 15:29:49 +0200

    Edit

--- LIbraries/game.py
+++ LIbraries/game.py
@@ -1,2 +1,27 @@
 import random
 
+while True:
+    try:
+        n = int(input("Level: "))
+        if n > 0: 
+            break
+    except ValueError:
+        pass
+
+
+target = random.randint(1, n)
+
+while True:
+    try:
+        guess = int(input("Guess: "))
+        if guess < 1:
+            continue
+        elif guess < target:
+            print("Too small!")
+        elif guess > target:
+            print("Too large!")
+        else:
+            print("Just right!")
+            break
+    except ValueError:
+        pass
