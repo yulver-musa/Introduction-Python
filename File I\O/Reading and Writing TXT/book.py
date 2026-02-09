21cc9789aa367045ae850b8d82b95bf8d22ca30a
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-02-09 10:24:59 +0200

    Update book.py

--- File I\O/Reading and Writing TXT/book.py
+++ File I\O/Reading and Writing TXT/book.py
@@ -1,1 +1,10 @@
+def main():
+    with open("alice.txt", "r") as f:
+        contents = f.readlines()
 
+    chapter1 = contents[52:272]
+    with open("chapter1.txt", "w") as f:
+        f.writelines(chapter1)
+
+
+main()
