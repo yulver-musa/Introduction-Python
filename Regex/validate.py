7ad74b8081337bc40c2e16ac92e08b94cbd504c9
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-02-14 13:03:41 +0200

    Regex version

--- Regex/validate.py
+++ Regex/validate.py
@@ -1,6 +1,13 @@
+import re
+
 email = input("What's your email? ").strip()
 
-if "@" in email:
+#if "@" in email:
+#    print("Valid")
+#else:
+#    print("Invalid")
+
+if re.search(r"^[^@]+@[^@]+\.com$", email):
     print("Valid")
 else:
     print("Invalid")
