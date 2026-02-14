1e268bccb119f2305ee7f3c586a356bc8e42b0b7
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-02-14 14:15:26 +0200

    Second version

--- Regex/format.py
+++ Regex/format.py
@@ -2,7 +2,7 @@ import re
 
 name = input("What's your name? ").strip()
 
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), *(.+)$", name)
 if matches:
     last, first = matches.groups()
     name = f"{first} {last}"
