3b775586b696f8e7a5eb986035a68d6cdff09f93
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-02-14 14:51:55 +0200

    New Version

--- Regex/extract.py
+++ Regex/extract.py
@@ -2,5 +2,6 @@ import re
 
 url = input("URL: ").strip()
 
-username = re.sub(r"https://twitter.com/", "", url)
-print(f"Username: {username}")
+matches = re.search(r"^https?://(www\.)?twitter\.com/(.+)$", "", re.IGNORECASE)
+if matches:
+    print(f"Username:", matches.group(2))
