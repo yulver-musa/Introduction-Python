b7242a958d0f8d6928c3f3d78d6b192c2aa9709d
Yulver Musa <165466040+yulver-musa@users.noreply.github.com>
2026-02-15 19:02:37 +0200

    New

---
+++ Regex/groups.py
@@ -0,0 +1,19 @@
+import re
+
+
+locations = {"+1": "United States and Canada", "+62": "Indonesia", "+505": "Nicaragua"}
+
+
+def main():
+    pattern = r"(?P<country_code>\+\d{1,3}) \d{3}-\d{3}-\d{4}"
+    number = input("Number: ")
+
+    match = re.search(pattern, number)
+    if match:
+        country_code = match.group("country_code")
+        print(locations[country_code])
+    else:
+        print("Unknown")
+
+
+main()
